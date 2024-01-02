76e7b8be2fbc4cfc5f1eef4d0635e0416edc2302
THGeek <alex24ht@hotmail>
2024-01-02 13:13:48 +0800

    feat: add another solution with less time complexity for LeetCode 855

---
+++ src/leetcode/ExamRoot_2_855.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2024 THGEEK.COM. All rights reserved.
+ */
+
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeSet;
+
+/**
+ * @author Tao Hong
+ * @version 1.0
+ * @since 2024/01/02 13:11
+ */
+public class ExamRoot_2_855 {
+    // 将端点 p 映射到以 p 为左端点的线段
+    private final Map<Integer, int[]> startMap;
+    // 将端点 p 映射到以 p 为右端点的线段
+    private final Map<Integer, int[]> endMap;
+    // 根据线段长度从小到大存放所有线段
+    private final TreeSet<int[]> pq;
+    private final int capacity;
+
+    public ExamRoot_2_855(int capacity) {
+        this.capacity = capacity;
+        startMap = new HashMap<>();
+        endMap = new HashMap<>();
+        pq = new TreeSet<>((a, b) -> {
+            int distA = distance(a);
+            int distB = distance(b);
+            // 如果长度相同，就比较索引
+            if (distA == distB) return b[0] - a[0];
+            return distA - distB;
+        });
+        // 在有序集合中先放一个虚拟线段
+        addInterval(new int[]{-1, capacity});
+    }
+
+    /* 去除一个线段 */
+    private void removeInterval(int[] intv) {
+        pq.remove(intv);
+        startMap.remove(intv[0]);
+        endMap.remove(intv[1]);
+    }
+
+    /* 增加一个线段 */
+    private void addInterval(int[] intv) {
+        pq.add(intv);
+        startMap.put(intv[0], intv);
+        endMap.put(intv[1], intv);
+    }
+
+    private int distance(int[] intv) {
+        int x = intv[0];
+        int y = intv[1];
+        if (x == -1) return y;
+        if (y == capacity) return capacity - 1 - x;
+        // 中点和端点之间的长度
+        return (y - x) / 2;
+    }
+
+    public int seat() {
+        // 从有序集合拿出最长的线段
+        int[] longest = pq.last();
+        int x = longest[0];
+        int y = longest[1];
+        int seat;
+        if (x == -1) { // 情况一
+            seat = 0;
+        } else if (y == capacity) { // 情况二
+            seat = capacity - 1;
+        } else { // 情况三
+            seat = (y - x) / 2 + x;
+        }
+        // 将最长的线段分成两段
+        int[] left = new int[]{x, seat};
+        int[] right = new int[]{seat, y};
+        removeInterval(longest);
+        addInterval(left);
+        addInterval(right);
+        return seat;
+    }
+
+    public void leave(int p) {
+        // 将 p 左右的线段找出来
+        int[] right = startMap.get(p);
+        int[] left = endMap.get(p);
+        // 合并两个线段成为一个线段
+        int[] merged = new int[]{left[0], right[1]};
+        removeInterval(left);
+        removeInterval(right);
+        addInterval(merged);
+    }
+}
---
+++ test/leetcode/ExamRoom_2_855Test.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2023 THGEEK.COM. All rights reserved.
+ */
+
+package leetcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Test for {@link ExamRoom_855}
+ *
+ * @author Tao Hong
+ * @version 1.0
+ * @since 2023/12/30 11:13
+ */
+class ExamRoom_2_855Test {
+    private ExamRoot_2_855 examRoom;
+
+    @BeforeEach
+    void setUp() {
+        examRoom = new ExamRoot_2_855(10);
+    }
+
+    @Test
+    void seatAndLeave() {
+        int zero = examRoom.seat();
+        assertEquals(0, zero);
+        int nine = examRoom.seat();
+        assertEquals(9, nine);
+        int four = examRoom.seat();
+        assertEquals(4, four);
+        int two = examRoom.seat();
+        assertEquals(2, two);
+
+        examRoom.leave(4);
+
+        int five = examRoom.seat();
+        assertEquals(5, five);
+    }
+}
