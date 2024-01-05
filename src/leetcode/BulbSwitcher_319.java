408a3cfb91bf65778b25bc8a3479dfc85516314f
THGeek <alex24ht@hotmail>
2024-01-05 11:04:15 +0800

    feat: add solution for LeetCode 319

---
+++ src/leetcode/BulbSwitcher_319.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2024 THGEEK.COM. All rights reserved.
+ */
+
+package leetcode;
+
+/**
+ * LeetCode 319
+ *
+ * @author Tao Hong
+ * @version 1.0
+ * @since 2024/01/05 10:09
+ */
+public class BulbSwitcher_319 {
+    public int bulbSwitch(int n) {
+        return 0;
+    }
+}
---
+++ test/leetcode/BulbSwitcher_319Test.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2024 THGEEK.COM. All rights reserved.
+ */
+
+package leetcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test for {@link BulbSwitcher_319}
+ *
+ * @author Tao Hong
+ * @version 1.0
+ * @since 2024/01/05 10:49
+ */
+class BulbSwitcher_319Test {
+    private BulbSwitcher_319 test;
+
+    @BeforeEach
+    void setUp() {
+        test = new BulbSwitcher_319();
+    }
+
+    @Test
+    void zeroBulbs() {
+        int zero = test.bulbSwitch(0);
+        assertEquals(0, zero);
+    }
+
+    @Test
+    void oneBulb() {
+        int one = test.bulbSwitch(1);
+        assertEquals(1, one);
+    }
+}
