4fc29d01d18ad2a04219e96decdb84def6595509
THGeek <alex24ht@hotmail>
2024-01-08 17:16:25 +0800

    feat: add another solution for LeetCode 338

--- src/leetcode/CountingBits_338.java
+++ src/leetcode/CountingBits_338.java
@@ -4,29 +4,62 @@
 
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * LeetCode 338
  *
  * @author Tao Hong
  * @version 1.0
- * @since 2024/01/08 14:19
+ * @since 2024/01/08 17:14
  */
-public class CountingBits_338 {
-    public int[] countBits(int n) {
-        int[] res = new int[n + 1];
+public interface CountingBits_338 {
+    int[] countBits(int n);
 
-        for (int i = 1; i < n + 1; i++) {
-            int count = 0;
-            int mask = 1 << 19;
-            for (int j = 0; j < 20; j++) {
-                if ((i & mask) > 0) {
-                    count++;
+    class Solution1 implements CountingBits_338 {
+        @Override
+        public int[] countBits(int n) {
+            int[] res = new int[n + 1];
+
+            for (int i = 1; i < n + 1; i++) {
+                int count = 0;
+                int mask = 1 << 19;
+                for (int j = 0; j < 20; j++) {
+                    if ((i & mask) > 0) {
+                        count++;
+                    }
+                    mask = mask >> 1;
                 }
-                mask = mask >> 1;
+                res[i] = count;
             }
-            res[i] = count;
+
+            return res;
+        }
+    }
+
+    class Solution2 implements CountingBits_338 {
+        @Override
+        public int[] countBits(int n) {
+            // dp[i] = dp[i-offset] + 1
+            int[] res = new int[n + 1];
+            List<Integer> powersOfTwo = new ArrayList<>();
+            powersOfTwo.add(0);
+
+            int i = 1;
+            while (i <= n) {
+                if (isPowerOfTwo(i)) {
+                    powersOfTwo.add(i);
+                }
+                int lastPowerOfTwo = powersOfTwo.get(powersOfTwo.size() - 1);
+                res[i] = res[i - lastPowerOfTwo] + 1;
+                i++;
+            }
+            return res;
         }
 
-        return res;
+        private boolean isPowerOfTwo(int num) {
+            return (num & (num - 1)) == 0;
+        }
     }
 }
--- test/leetcode/CountingBits_338Test.java
+++ test/leetcode/CountingBits_338Test.java
@@ -7,7 +7,10 @@ package leetcode;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 /**
  * Test for {@link CountingBits_338}
@@ -17,25 +20,29 @@ import static org.junit.jupiter.api.Assertions.*;
  * @since 2024/01/08 14:22
  */
 class CountingBits_338Test {
-    private CountingBits_338 test;
-
+    private List<CountingBits_338> tests;
     private final int[] expectedBitsInTwo = {0, 1, 1};
     private final int[] expectedBitsInFive = {0, 1, 1, 2, 1, 2};
 
     @BeforeEach
     void setUp() {
-        test = new CountingBits_338();
+        tests = Arrays.asList(new CountingBits_338.Solution1(),
+                new CountingBits_338.Solution2());
     }
 
     @Test
     void countForTwo() {
-        int[] bitsInTwo = test.countBits(2);
-        assertArrayEquals(expectedBitsInTwo, bitsInTwo);
+        for (CountingBits_338 test : tests) {
+            int[] bitsInTwo = test.countBits(2);
+            assertArrayEquals(expectedBitsInTwo, bitsInTwo);
+        }
     }
 
     @Test
     void countForFive() {
-        int[] bitsInFive = test.countBits(5);
-        assertArrayEquals(expectedBitsInFive, bitsInFive);
+        for (CountingBits_338 test : tests) {
+            int[] bitsInFive = test.countBits(5);
+            assertArrayEquals(expectedBitsInFive, bitsInFive);
+        }
     }
 }
