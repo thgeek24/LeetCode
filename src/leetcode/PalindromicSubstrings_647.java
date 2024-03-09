d965ea658a1e91b09d00da2a46f9c28b1d952a57
THGeek <alex24ht@hotmail>
2024-03-09 12:25:17 +0800

    feat: add the solution for LeetCode 647

---
+++ src/leetcode/PalindromicSubstrings_647.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2024 THGEEK.COM. All rights reserved.
+ */
+
+package leetcode;
+
+/**
+ * LeetCode 647
+ *
+ * @author Tao Hong
+ * @version 1.0
+ * @since 2024/03/09 12:02
+ */
+public class PalindromicSubstrings_647 {
+    public int countSubstrings(String s) {
+        int len = s.length();
+        boolean[][] dp = new boolean[len][len];
+
+        int cnt = 0;
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+            cnt++;
+        }
+        for (int i = 0; i < len - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                cnt++;
+            }
+        }
+        for (int j = 2; j < len; j++) {
+            for (int i = 0; i < j; i++) {
+                if (dp[i + 1][j - 1] && s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = true;
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+}
