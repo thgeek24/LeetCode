6386660b74e6d1aaf297692dd69090f19c94c6cd
THGeek <alex24ht@hotmail>
2024-03-28 16:36:30 +0800

    feat: add another solution for LeetCode 1155

--- test/leetcode/NumberOfDiceRollsWithTargetSum_1155.java
+++ test/leetcode/NumberOfDiceRollsWithTargetSum_1155.java
@@ -36,4 +36,22 @@ public class NumberOfDiceRollsWithTargetSum_1155 {
 
         return (int) (dp[n][target] % mod);
     }
+
+    public int numRollsToTarget_2(int n, int f, int target) {
+        final int MOD = 1000000007;
+        int[][] dp = new int[n + 1][target + 1];
+        dp[0][0] = 1;  // Base case
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= target; j++) {
+                for (int k = 1; k <= f; k++) {
+                    if (j - k >= 0) {
+                        dp[i][j] = (dp[i][j] + dp[i - 1][j - k]) % MOD;
+                    }
+                }
+            }
+        }
+
+        return dp[n][target];
+    }
 }
