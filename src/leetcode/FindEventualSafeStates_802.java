651de6ecf25bb4138d646682dd2ecc2808701418
THGeek <alex24ht@hotmail>
2024-06-20 18:31:16 +0800

    feat: add solution for LeetCode 802

---
+++ src/leetcode/FindEventualSafeStates_802.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2024 THGEEK.COM. All rights reserved.
+ */
+
+package leetcode;
+
+import java.util.List;
+
+/**
+ * LeetCode 802
+ *
+ * @author Tao Hong
+ * @version 1.0
+ * @since 2024/06/20 17:54
+ */
+public class FindEventualSafeStates_802 {
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        return null;
+    }
+}
