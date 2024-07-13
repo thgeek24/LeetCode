11c24e2e0d6d97919a993bb9d2be217947219a0f
THGeek <alex24ht@hotmail>
2024-07-13 21:25:01 +0800

    feat: fix the solution for LeetCode 886

--- src/leetcode/PossibleBipartition_886.java
+++ src/leetcode/PossibleBipartition_886.java
@@ -16,80 +16,34 @@ import java.util.List;
  */
 public class PossibleBipartition_886 {
     public boolean possibleBipartition(int n, int[][] dislikes) {
-        if (n == 1) {
-            return false;
-        }
-        if (dislikes.length == 0) {
-            return true;
-        }
-        boolean[] visited = new boolean[n + 1];
-        UnionFind uf = new UnionFind(n);
-        List<List<Integer>> dislikeList = new ArrayList<>();
+        List<List<Integer>> graph = new ArrayList<>();
         for (int i = 0; i <= n; i++) {
-            dislikeList.add(new ArrayList<>());
+            graph.add(new ArrayList<>());
         }
-
         for (int[] dislike : dislikes) {
-            dislikeList.get(dislike[0]).add(dislike[1]);
+            graph.get(dislike[0]).add(dislike[1]);
+            graph.get(dislike[1]).add(dislike[0]);
         }
 
+        int[] colors = new int[n + 1];
         for (int i = 1; i <= n; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-
-                List<Integer> dislikedPeople = dislikeList.get(i);
-                if (dislikedPeople.isEmpty()) {
-                    continue;
-                }
-                for (Integer dislikedPerson : dislikedPeople) {
-                    if (uf.isConnected(i, dislikedPerson)) {
-                        return false;
-                    }
-                }
-                uf.unionAll(dislikedPeople);
+            if (colors[i] == 0 && !dfs(i, 1, colors, graph)) {
+                return false;
             }
         }
-
         return true;
     }
 
-    private static class UnionFind {
-        private final int[] roots;
-
-        public UnionFind(int n) {
-            this.roots = new int[n + 1];
-            for (int i = 0; i < n + 1; i++) {
-                roots[i] = i;
+    private boolean dfs(int node, int color, int[] colors, List<List<Integer>> graph) {
+        if (colors[node] != 0) {
+            return colors[node] == color;
+        }
+        colors[node] = color;
+        for (int neighbor : graph.get(node)) {
+            if (!dfs(neighbor, -color, colors, graph)) {
+                return false;
             }
         }
-
-        public void union(int p, int q) {
-            int rootP = getRoot(p);
-            int rootQ = getRoot(q);
-            if (rootP != rootQ) {
-                roots[rootQ] = rootP;
-            }
-        }
-
-        public void unionAll(List<Integer> points) {
-            if (points.size() < 2) {
-                return;
-            }
-            int firstPoint = points.get(0);
-            for (int i = 1; i < points.size(); i++) {
-                union(firstPoint, points.get(i));
-            }
-        }
-
-        public int getRoot(int n) {
-            if (roots[n] == n) {
-                return n;
-            }
-            return roots[n] = getRoot(roots[n]);
-        }
-
-        public boolean isConnected(int p, int q) {
-            return getRoot(p) == getRoot(q);
-        }
+        return true;
     }
 }
