b86725dba84608d11b135cfbc3438ec52acd9c69
THGeek <alex24ht@hotmail>
2024-08-02 14:38:54 +0800

    feat: add the solution for 76

--- src/leetcode/MinimumWindowSubstring_76.java
+++ src/leetcode/MinimumWindowSubstring_76.java
@@ -4,12 +4,8 @@
 
 package leetcode;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
-import java.util.Queue;
 
 /**
  * LeetCode 76
@@ -20,45 +16,44 @@ import java.util.Queue;
  */
 public class MinimumWindowSubstring_76 {
     public String minWindow(String s, String t) {
-        Map<Character, Integer> targetMap = new HashMap<>();
+        Map<Character, Integer> charMap = new HashMap<>();
         for (int i = 0; i < t.length(); i++) {
-            targetMap.put(t.charAt(i), targetMap.getOrDefault(t.charAt(i), 0) + 1);
+            int cnt = charMap.getOrDefault(t.charAt(i), 0);
+            charMap.put(t.charAt(i), cnt + 1);
         }
 
-        int leftMost = 0;
-        int right = 0;
+        int slow = 0;
+        int fast = 0;
+        int minLeft = slow;
         int minLen = Integer.MAX_VALUE;
-        Map<Character, List<Integer>> charMap = new HashMap<>();
-        Queue<Character> queue = new LinkedList<>();
-        while (right < s.length()) {
-            char ch = s.charAt(right);
-            if (targetMap.containsKey(ch)) {
-                queue.add(ch);
-                int targetCount = targetMap.get(ch);
-                List<Integer> indexes = charMap.getOrDefault(ch, new ArrayList<>());
-                indexes.add(right);
-                charMap.put(ch, indexes);
-
-                if (indexes.size() >= targetCount) {
-                    while (true) {
-                        Character deletingCh = queue.peek();
-                        List<Integer> deletingIndexes = charMap.get(deletingCh);
-                        if (deletingIndexes.size() > targetMap.get(deletingCh)) {
-                            deletingIndexes.remove(0);
-                            queue.poll();
-                        } else {
-                            leftMost = deletingIndexes.get(0);
-                            minLen = Math.min(minLen, right - leftMost + 1);
-                            break;
-                        }
-                    }
+        int remaining = t.length();
+        while (fast < s.length()) {
+            char char1 = s.charAt(fast);
+            if (charMap.containsKey(char1)) {
+                int fastCharCnt = charMap.get(char1);
+                if (fastCharCnt > 0) {
+                    remaining--;
                 }
+                charMap.put(char1, fastCharCnt - 1);
             }
-            right++;
+            fast++;
+
+            while (remaining == 0) {
+                if (fast - slow < minLen) {
+                    minLen = fast - slow;
+                    minLeft = slow;
+                }
+                char char2 = s.charAt(slow);
+                if (charMap.containsKey(char2)) {
+                    int slowCharCnt = charMap.get(char2);
+                    if (slowCharCnt >= 0) {
+                        remaining++;
+                    }
+                    charMap.put(char2, slowCharCnt + 1);
+                }
+                slow++;
+            }
         }
-        if (minLen == Integer.MAX_VALUE) {
-            return "";
-        }
-        return s.substring(leftMost, leftMost + minLen);
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(minLeft, minLeft + minLen);
     }
 }
