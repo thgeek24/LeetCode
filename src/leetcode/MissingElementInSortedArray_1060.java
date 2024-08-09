012794328ae98b684b7960e9dc59ed3770659d3b
THGeek <alex24ht@hotmail>
2024-08-09 14:10:27 +0800

    feat: add the solution for LeetCode 1060

--- src/leetcode/MissingElementInSortedArray_1060.java
+++ src/leetcode/MissingElementInSortedArray_1060.java
@@ -15,25 +15,30 @@ package leetcode;
  */
 public class MissingElementInSortedArray_1060 {
     public int missingElement(int[] nums, int k) {
+        int n = nums.length;
+        // If the k-th missing number is beyond the last element of the array
+        if (k > missing(nums, n - 1)) {
+            return nums[n - 1] + k - missing(nums, n - 1);
+        }
+
         int left = 0;
-        int right = nums.length - 1;
+        int right = n - 1;
+        // Binary search to find the smallest index where the missing count is >= k
         while (left < right) {
             int mid = left + (right - left) / 2;
-            int diff = nums[mid] - nums[left];
-            int indexDiff = mid - left;
-            if (diff == indexDiff) {
+            if (missing(nums, mid) < k) {
                 left = mid + 1;
-                int _diff = nums[left] - nums[mid];
-                if (k < _diff) {
-                    return nums[mid] + k;
-                } else {
-                    k -= _diff - 1;
-                }
             } else {
-                // diff > indexDiff, cause diff < indexDiff will not happen
                 right = mid;
             }
         }
-        return nums[left] + k;
+
+        // The k-th missing number is just before the index where the count of missing numbers is just enough
+        return nums[left - 1] + k - missing(nums, left - 1);
+    }
+
+    // Helper function to calculate the number of missing elements until index idx
+    private int missing(int[] nums, int idx) {
+        return nums[idx] - nums[0] - idx;
     }
 }
