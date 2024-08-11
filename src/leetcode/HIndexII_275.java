efb1766b8c6b5d34f8b60a0200bd100918c96590
THGeek <alex24ht@hotmail>
2024-08-11 11:51:28 +0800

    feat: add a failed solution for LeetCode 275

---
+++ src/leetcode/HIndexII_275.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2024 THGEEK.COM. All rights reserved.
+ */
+
+package leetcode;
+
+/**
+ * LeetCode 275
+ *
+ * @author Tao Hong
+ * @version 1.0
+ * @since 2024/08/11 10:33
+ */
+public class HIndexII_275 {
+    public int hIndex(int[] citations) {
+        int len = citations.length;
+        if (len == 1) {
+            return citations[0] > 0 ? 1 : 0;
+        }
+
+        int left = 0;
+        int right = len - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int papers = len - mid;
+            if (papers >= citations[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        int papers = len - left;
+        if (papers >= citations[left]) {
+            return citations[left];
+        }
+        return citations[left - 1];
+    }
+}
