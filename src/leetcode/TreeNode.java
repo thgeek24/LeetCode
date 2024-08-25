b8e11071e6d693525d05a87af38bcd62daa6a74d
THGeek <alex24ht@hotmail>
2024-08-25 21:07:12 +0800

    feat: add the solution for LeetCode 226

--- src/leetcode/TreeNode.java
+++ src/leetcode/TreeNode.java
@@ -8,11 +8,14 @@ public class TreeNode {
     TreeNode left;
     TreeNode right;
 
+    TreeNode() {
+    }
+
     TreeNode(int x) {
         val = x;
     }
 
-    public TreeNode(int val, TreeNode left, TreeNode right) {
+    TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
         this.right = right;
