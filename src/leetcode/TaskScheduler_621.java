a5b839974f4cdbc02bb013efeec51375427c92c3
THGeek <alex24ht@hotmail>
2024-08-31 17:08:25 +0800

    feat: add the solution for LeetCode 621

--- src/leetcode/TaskScheduler_621.java
+++ src/leetcode/TaskScheduler_621.java
@@ -4,6 +4,10 @@
 
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
 /**
  * LeetCode 621
  *
@@ -12,6 +16,14 @@ package leetcode;
  * @since 2024/08/31 15:54
  */
 public class TaskScheduler_621 {
+    private static class IndexCount {
+        private int count;
+
+        public IndexCount(int index, int count) {
+            this.count = count;
+        }
+    }
+
     public int leastInterval(char[] tasks, int n) {
         if (n == 0) {
             return tasks.length;
@@ -22,27 +34,38 @@ public class TaskScheduler_621 {
             int index = task - 'A';
             charCount[index]++;
         }
+        PriorityQueue<IndexCount> queue = new PriorityQueue<>(((o1, o2) -> o2.count - o1.count));
+        for (int i = 0; i < charCount.length; i++) {
+            if (charCount[i] > 0) {
+                queue.offer(new IndexCount(i, charCount[i]));
+            }
+        }
 
         final int groupSize = n + 1;
         int intervals = 0;
         int remaining = tasks.length;
         while (remaining > 0) {
-            int completed = 0;
-            for (int i = 0; i < 26; i++) {
-                if (charCount[i] > 0) {
-                    charCount[i]--;
-                    remaining--;
-                    completed++;
-                    intervals++;
-                    if (remaining == 0) {
-                        return intervals;
-                    }
-                    if (completed == groupSize) {
-                        break;
-                    }
+            List<IndexCount> used = new ArrayList<>();
+            while (used.size() < groupSize) {
+                IndexCount item = queue.poll();
+                if (item == null) {
+                    break;
+                }
+                item.count--;
+                remaining--;
+                intervals++;
+                used.add(item);
+                if (remaining == 0) {
+                    return intervals;
                 }
             }
-            intervals += groupSize - completed;
+
+            intervals += groupSize - used.size();
+            for (IndexCount item : used) {
+                if (item.count > 0) {
+                    queue.offer(item);
+                }
+            }
         }
 
         return intervals;
