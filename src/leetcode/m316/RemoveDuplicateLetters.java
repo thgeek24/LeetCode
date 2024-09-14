6e16a4b27eaa3747a60a258659ae11d22f571c4c
THGeek <alex24ht@hotmail>
2024-09-14 11:13:26 +0800

    feat: add the solution for LeetCode 316

--- src/leetcode/m316/RemoveDuplicateLetters.java
+++ src/leetcode/m316/RemoveDuplicateLetters.java
@@ -4,7 +4,7 @@
 
 package leetcode.m316;
 
-import java.util.LinkedList;
+import java.util.Stack;
 
 /**
  * LeetCode 316
@@ -15,26 +15,47 @@ import java.util.LinkedList;
  */
 public class RemoveDuplicateLetters {
     public String removeDuplicateLetters(String s) {
-        LinkedList<Character> letters = new LinkedList<>();
-        for (int i = 0; i < s.length(); i++) {
-            char letter = s.charAt(i);
-            if (!letters.contains(letter)) {
-                letters.add(letter);
+        // Array to count the frequency of each character in the string
+        int[] freq = new int[26];
+        // Array to check if a character is already in the result
+        boolean[] visited = new boolean[26];
+
+        // Count the frequency of each character
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        // Stack to store the result characters
+        Stack<Character> stack = new Stack<>();
+
+        // Iterate through each character in the string
+        for (char c : s.toCharArray()) {
+            // Decrease the frequency of the current character
+            freq[c - 'a']--;
+
+            // If the character is already in the result, skip it
+            if (visited[c - 'a']) {
                 continue;
             }
 
-            int index = letters.indexOf(letter);
-            if (index != letters.size() - 1) {
-                Character nextLetter = letters.get(index + 1);
-                if (nextLetter < letter) {
-                    letters.remove(index);
-                    letters.add(letter);
-                }
+            // Ensure lexicographically smaller result:
+            // Pop the stack if the current character is smaller than the top of the stack
+            // and the top character still appears later in the string
+            while (!stack.isEmpty() && c < stack.peek() && freq[stack.peek() - 'a'] > 0) {
+                visited[stack.pop() - 'a'] = false;
             }
+
+            // Push the current character onto the stack and mark it as visited
+            stack.push(c);
+            visited[c - 'a'] = true;
         }
 
-        StringBuilder sb = new StringBuilder();
-        letters.forEach(sb::append);
-        return sb.toString();
+        // Build the result string from the stack
+        StringBuilder result = new StringBuilder();
+        for (char c : stack) {
+            result.append(c);
+        }
+
+        return result.toString();
     }
 }
