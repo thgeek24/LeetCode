5ff14c3dd9ab2a9bf813e19289f7a4f95bbc0d4a
THGeek <alex24ht@hotmail>
2024-10-11 10:28:08 +0800

    feat: add the correct solution for LeetCode 236

--- src/leetcode/m236/LowestCommonAncestorOfABinaryTree.go
+++ src/leetcode/m236/LowestCommonAncestorOfABinaryTree.go
@@ -10,47 +10,24 @@ type TreeNode struct {
  * LeetCode 236
  */
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-	if p.Val == root.Val || q.Val == root.Val {
+	// Base case: if the current node is null or matches either p or q, return the current node
+	if root == nil || root == p || root == q {
 		return root
 	}
 
-	isPLeft := true
-	pDepth, pPrev := findDepth(root.Left, root, p.Val, 1)
-	if pDepth == -1 {
-		isPLeft = false
-		pDepth, pPrev = findDepth(root.Right, root, p.Val, 1)
-	}
+	// Recur for left and right subtrees
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
 
-	isQLeft := true
-	qDepth, _ := findDepth(root.Left, root, q.Val, 1)
-	if qDepth == -1 {
-		isQLeft = false
-		qDepth, _ = findDepth(root.Right, root, q.Val, 1)
-	}
-
-	if isPLeft != isQLeft {
+	// If both left and right are non-null, current node is the LCA
+	if left != nil && right != nil {
 		return root
 	}
 
-	if pDepth < qDepth {
-		return p
-	} else if pDepth > qDepth {
-		return q
-	} else {
-		return pPrev
+	// Otherwise, return non-null node (either left or right)
+	if left != nil {
+		return left
 	}
-}
 
-func findDepth(curr, prev *TreeNode, target, depth int) (int, *TreeNode) {
-	if curr == nil {
-		return -1, prev
-	}
-	if curr.Val == target {
-		return depth, prev
-	}
-	leftDepth, LeftPrev := findDepth(curr.Left, curr, target, depth+1)
-	if leftDepth != -1 {
-		return leftDepth, LeftPrev
-	}
-	return findDepth(curr.Right, curr, target, depth+1)
+	return right
 }
