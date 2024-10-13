fa6b0e6eddc4c9ebe5812dc1ab22b21f0501abf6
THGeek <alex24ht@hotmail>
2024-10-13 21:41:17 +0800

    feat: optimise the solution of LeetCode SQL 1193

--- src/leetcode/m1193/MonthlyTransactionsI.sql
+++ src/leetcode/m1193/MonthlyTransactionsI.sql
@@ -2,10 +2,12 @@
  * Copyright (c) 2024 THGEEK.COM. All rights reserved.
  */
 
-select DATE_FORMAT(trans_date, '%Y-%m') AS month, country,
-count(*) as trans_count,
-sum(case when state ="approved" then 1 else 0 end) as approved_count,
-sum(amount) as trans_total_amount,
-sum(case when state = "approved" then amount else 0 end) as approved_total_amount
-from Transactions
-group by DATE_FORMAT(trans_date, '%Y-%m'), country;
+SELECT
+    DATE_FORMAT(trans_date, '%Y-%m') AS month,
+    country,
+    COUNT(*) AS trans_count,
+    SUM(state = 'approved') AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(IF(state = 'approved', amount, 0)) AS approved_total_amount
+FROM Transactions
+GROUP BY month, country;
