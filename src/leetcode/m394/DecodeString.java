79922191d7d04a8170758cc8f22204ba3a293ed4
THGeek <alex24ht@hotmail>
2024-11-05 12:59:36 +0800

    feat: add the solution of LeetCode 394

---
+++ src/leetcode/m394/DecodeString.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2024 THGEEK.COM. All rights reserved.
+ */
+
+package leetcode.m394;
+
+import java.util.Stack;
+
+/**
+ * LeetCode 394
+ *
+ * @author Tao Hong
+ * @version 1.0
+ * @since 2024/11/05 12:30
+ */
+public class DecodeString {
+    public String decodeString(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == ']') {
+                StringBuilder charBuilder = new StringBuilder();
+                while (stack.peek() != '[') {
+                    charBuilder.append(stack.pop());
+                }
+                charBuilder.reverse();
+                stack.pop();
+
+                StringBuilder intBuilder = new StringBuilder();
+                while (!stack.isEmpty() && isDigit(stack.peek())) {
+                    intBuilder.append(stack.pop());
+                }
+                intBuilder.reverse();
+                int count = intBuilder.length() == 0 ? 1 : Integer.parseInt(intBuilder.toString());
+
+                while (count > 0) {
+                    for (int i = 0; i < charBuilder.length(); i++) {
+                        stack.push(charBuilder.charAt(i));
+                    }
+                    count--;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        stack.forEach(sb::append);
+        return sb.toString();
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+}
