1aec4b67811bdd3a5e264eaa28055a022573f266
THGeek <alex24ht@hotmail>
2024-11-06 16:24:09 +0800

    feat: add the solution for LeetCode 2462

--- src/leetcode/m2462/TotalCostToHireKWorkers.java
+++ src/leetcode/m2462/TotalCostToHireKWorkers.java
@@ -5,7 +5,6 @@
 package leetcode.m2462;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.PriorityQueue;
 
 /**
@@ -28,43 +27,36 @@ public class TotalCostToHireKWorkers {
 
         int left = candidates - 1;
         int right = costs.length - candidates;
-        Comparator<int[]> comparator = (o1, o2) -> {
-            if (o1[0] == o2[0]) {
-                return o1[1] - o2[1];
-            } else {
-                return o1[0] - o2[0];
-            }
-        };
-        PriorityQueue<int[]> leftQueue = new PriorityQueue<>(comparator);
-        PriorityQueue<int[]> rightQueue = new PriorityQueue<>(comparator);
+        PriorityQueue<Integer> leftQueue = new PriorityQueue<>();
+        PriorityQueue<Integer> rightQueue = new PriorityQueue<>();
         for (int i = 0; i <= left; i++) {
-            leftQueue.add(new int[]{costs[i], i});
+            leftQueue.add(costs[i]);
         }
         for (int i = right; i < costs.length; i++) {
-            rightQueue.add(new int[]{costs[i], i});
+            rightQueue.add(costs[i]);
         }
         long cost = 0;
         while (left < right && k > 0) {
-            int[] worker1 = leftQueue.peek();
-            int[] worker2 = rightQueue.peek();
-            if (worker1[0] <= worker2[0]) {
+            int worker1 = leftQueue.peek();
+            int worker2 = rightQueue.peek();
+            if (worker1 <= worker2) {
                 leftQueue.poll();
-                cost += worker1[0];
+                cost += worker1;
                 k--;
                 left++;
                 if (left == right) {
                     break;
                 }
-                leftQueue.offer(new int[]{costs[left], left});
+                leftQueue.offer(costs[left]);
             } else {
                 rightQueue.poll();
-                cost += worker2[0];
+                cost += worker2;
                 k--;
                 right--;
                 if (left == right) {
                     break;
                 }
-                rightQueue.offer(new int[]{costs[right], right});
+                rightQueue.offer(costs[right]);
             }
         }
         if (k == 0) {
@@ -72,8 +64,7 @@ public class TotalCostToHireKWorkers {
         }
         leftQueue.addAll(rightQueue);
         while (k > 0) {
-            int[] worker = leftQueue.poll();
-            cost += worker[0];
+            cost += leftQueue.poll();
             k--;
         }
         return cost;
