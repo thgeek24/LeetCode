b74ab02a54f2286c411959cc1d2a5baea00b9d1f
THGeek <alex24ht@hotmail>
2024-11-06 21:55:37 +0800

    feat: add the solution for LeetCode 790

--- src/leetcode/m790/DominoAndTrominoTiling.java
+++ src/leetcode/m790/DominoAndTrominoTiling.java
@@ -13,16 +13,13 @@ package leetcode.m790;
  */
 public class DominoAndTrominoTiling {
     public int numTilings(int n) {
-        if (n == 1) return 1;
-        if (n == 2) return 2;
-        if (n == 3) return 5;
-        long[] dp = new long[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 5;
-        for (int i = 4; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        long[] dp = new long[n + 3];
+        dp[0] = 1;
+        dp[1] = 2;
+        dp[2] = 5;
+        for (int i = 3; i < n; i++) {
+            dp[i] = (dp[i - 1] * 2 + dp[i - 3]) % 1000000007;
         }
-        return (int) dp[n] % 1000000007;
+        return (int) dp[n - 1];
     }
 }
