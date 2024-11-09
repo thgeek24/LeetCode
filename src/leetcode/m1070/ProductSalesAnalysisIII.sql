a194510a8af58f95e3d0bd5fd3312608840c706f
THGeek <alex24ht@hotmail>
2024-11-09 16:49:44 +0800

    feat: add another SQL solution of LeetCode 1070

--- src/leetcode/m1070/ProductSalesAnalysisIII.sql
+++ src/leetcode/m1070/ProductSalesAnalysisIII.sql
@@ -2,8 +2,18 @@
  * Copyright (c) 2024 THGEEK.COM. All rights reserved.
  */
 
+-- Solution 1
 with FirstYearSale as (select product_id, MIN(year) as first_year from Sales group by product_id)
 select s.product_id, f.first_year, s.quantity, s.price
 from Sales s
          inner join FirstYearSale f
                     on s.product_id = f.product_id and s.year = f.first_year;
+
+-- Solution 2
+SELECT product_id, year AS first_year, quantity, price
+FROM Sales
+WHERE (product_id, year) IN (
+    SELECT product_id, MIN(year) AS year
+    FROM Sales
+    GROUP BY product_id
+);
