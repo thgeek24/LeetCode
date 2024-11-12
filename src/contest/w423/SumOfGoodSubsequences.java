acc6bbc472a9d65b1b21d16f712cbf17b6c5615e
THGeek <alex24ht@hotmail>
2024-11-12 10:44:50 +0800

    feat: add a time-limit-exceeded solution of LeetCode 3351

--- src/contest/w423/SumOfGoodSubsequences.java
+++ src/contest/w423/SumOfGoodSubsequences.java
@@ -4,29 +4,40 @@
 
 package contest.w423;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
+ * LeetCode 3351
+ *
  * @author Tao Hong
  * @version 1.0
  * @since 2024/11/10 11:36
  */
 public class SumOfGoodSubsequences {
     public int sumOfGoodSubsequences(int[] nums) {
-        int len = nums.length;
-        int[][][] dp = new int[len][len][2];
-        int sum = 0;
-        for (int i = 0; i < len; i++) {
-            dp[i][i][0] = nums[i];
-            dp[i][i][1] = nums[i];
-            sum += dp[i][i][0];
+        if (nums.length == 1) {
+            return nums[0];
         }
-        for (int k = 1; k < len; k++) {
-            for (int i = 0; i + k < len; i++) {
-                int j = i + k;
-                if (Math.abs(nums[j] - nums[j - 1]) == 1) {
 
+        int res = 0;
+        int MOD = 1000000007;
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < nums.length; i++) {
+            queue.offer(new int[]{nums[i], nums[i], i});
+            res = (res + nums[i]) % MOD;
+        }
+        while (!queue.isEmpty()) {
+            int[] item = queue.poll();
+            for (int i = item[2] + 1; i < nums.length; i++) {
+                int abs = Math.abs(item[1] - nums[i]);
+                if (abs == 1) {
+                    int sum = item[0] + nums[i];
+                    queue.offer(new int[]{sum, nums[i], i});
+                    res = (res + sum) % MOD;
                 }
             }
         }
-        return sum;
+        return res;
     }
 }
