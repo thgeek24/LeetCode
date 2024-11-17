2d0c5b9de78b55ff1c54a11f3e9f47e6b9d7394a
THGeek <alex24ht@hotmail>
2024-11-17 12:12:25 +0800

    feat: add a time-limited-exceeded solution of Weekly Contest Q3

---
+++ src/contest/w424/ZeroArrayTransformationII.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2024 THGEEK.COM. All rights reserved.
+ */
+
+package contest.w424;
+
+/**
+ * @author Tao Hong
+ * @version 1.0
+ * @since 2024/11/17 10:53
+ */
+public class ZeroArrayTransformationII {
+    public int minZeroArray(int[] nums, int[][] queries) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum == 0) {
+            return 0;
+        }
+
+        int k = 0;
+        for (int[] query : queries) {
+            int l = query[0], r = query[1];
+            for (int i = l; i <= r; i++) {
+                int decrement = Math.min(nums[i], query[2]);
+                nums[i] -= decrement;
+                sum -= decrement;
+            }
+            k++;
+            if (sum == 0) {
+                return k;
+            }
+        }
+        return -1;
+    }
+}
