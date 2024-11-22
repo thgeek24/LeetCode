c32c6e95f96e085cbe54164b439db85cb84682ed
THGeek <alex24ht@hotmail>
2024-11-22 14:40:47 +0800

    feat: add another solution of LeetCode 3357

--- test/contest/w424/MinimizeTheMaximumAdjacentElementDifferenceTest.java
+++ test/contest/w424/MinimizeTheMaximumAdjacentElementDifferenceTest.java
@@ -7,7 +7,9 @@ package contest.w424;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Test for {@link MinimizeTheMaximumAdjacentElementDifference}
@@ -17,31 +19,31 @@ import static org.junit.jupiter.api.Assertions.*;
  * @since 2024/11/20 08:34
  */
 class MinimizeTheMaximumAdjacentElementDifferenceTest {
-    private MinimizeTheMaximumAdjacentElementDifference test;
+    private List<MinimizeTheMaximumAdjacentElementDifference> tests;
 
     @BeforeEach
     void setUp() {
-        test = new MinimizeTheMaximumAdjacentElementDifference();
+        tests = MinimizeTheMaximumAdjacentElementDifference.getAllSolutions();
     }
 
     @Test
     void equalsFour() {
         int[] nums = {1, 2, -1, 10, 8};
         int expected = 4;
-        assertEquals(expected, test.minDifference(nums));
+        tests.forEach(test -> assertEquals(expected, test.minDifference(nums)));
     }
 
     @Test
     void equalsZero() {
         int[] nums = {-1, -1, -1};
         int expected = 0;
-        assertEquals(expected, test.minDifference(nums));
+        tests.forEach(test -> assertEquals(expected, test.minDifference(nums)));
     }
 
     @Test
     void equalsOne() {
         int[] nums = {-1, 10, -1, 8};
         int expected = 1;
-        assertEquals(expected, test.minDifference(nums));
+        tests.forEach(test -> assertEquals(expected, test.minDifference(nums)));
     }
 }
