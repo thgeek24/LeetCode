f2df83cb222d3e9cb68220ad4ec918d7b21f49de
THGeek <alex24ht@hotmail>
2024-12-16 10:31:55 +0800

    feat: add another solution of LeetCode 3388

--- test/contest/w428/CountBeautifulSplitsInAnArrayTest.java
+++ test/contest/w428/CountBeautifulSplitsInAnArrayTest.java
@@ -7,7 +7,10 @@ package contest.w428;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Test for {@link CountBeautifulSplitsInAnArray}
@@ -17,45 +20,45 @@ import static org.junit.jupiter.api.Assertions.*;
  * @since 2024/12/15 11:31
  */
 class CountBeautifulSplitsInAnArrayTest {
-    private CountBeautifulSplitsInAnArray test;
+    private List<CountBeautifulSplitsInAnArray> tests;
 
     @BeforeEach
     void setUp() {
-        test = new CountBeautifulSplitsInAnArray();
+        tests = Arrays.asList(new CountBeautifulSplitsInAnArray.Solution1(), new CountBeautifulSplitsInAnArray.Solution2());
     }
 
     @Test
     void test1() {
         int[] nums = {1, 1, 2, 1};
         int expected = 2;
-        assertEquals(expected, test.beautifulSplits(nums));
+        tests.forEach(test -> assertEquals(expected, test.beautifulSplits(nums)));
     }
 
     @Test
     void test2() {
         int[] nums = {1, 2, 3, 4};
         int expected = 0;
-        assertEquals(expected, test.beautifulSplits(nums));
+        tests.forEach(test -> assertEquals(expected, test.beautifulSplits(nums)));
     }
 
     @Test
     void test3() {
         int[] nums = {1, 2, 3, 1, 2, 3, 4, 5, 7, 8};
         int expected = 4;
-        assertEquals(expected, test.beautifulSplits(nums));
+        tests.forEach(test -> assertEquals(expected, test.beautifulSplits(nums)));
     }
 
     @Test
     void test4() {
         int[] nums = {2, 3, 2, 2, 1};
         int expected = 1;
-        assertEquals(expected, test.beautifulSplits(nums));
+        tests.forEach(test -> assertEquals(expected, test.beautifulSplits(nums)));
     }
 
     @Test
     void test5() {
         int[] nums = {3, 3, 3, 1, 3};
         int expected = 3;
-        assertEquals(expected, test.beautifulSplits(nums));
+        tests.forEach(test -> assertEquals(expected, test.beautifulSplits(nums)));
     }
 }
