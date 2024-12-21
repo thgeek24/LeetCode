18eb0b4bfc34b5f67d066cfdd330c77a067be733
THGeek <alex24ht@hotmail>
2024-12-21 10:06:33 +0800

    feat: add the in-place solution in GO for LeetCode 189

--- src/leetcode/m189/RotateArray.go
+++ src/leetcode/m189/RotateArray.go
@@ -1,7 +1,14 @@
 package m189
 
-// LeetCode 189
-func rotate(nums []int, k int) {
+// Rotator interface
+type Rotator interface {
+	Rotate(nums []int, k int)
+}
+
+// Solution1 implements Rotator interface
+type Solution1 struct{}
+
+func (s Solution1) Rotate(nums []int, k int) {
 	n := len(nums)
 	k = k % n
 	res := make([]int, n)
@@ -14,4 +21,24 @@ func rotate(nums []int, k int) {
 		j++
 	}
 	copy(nums, res)
+}
+
+// Solution2 implements Rotator interface
+type Solution2 struct{}
+
+func (s Solution2) Rotate(nums []int, k int) {
+	n := len(nums)
+	k = k % n
+
+	reverse(nums, 0, n-1)
+	reverse(nums, 0, k-1)
+	reverse(nums, k, n-1)
+}
+
+func reverse(nums []int, start, end int) {
+	for start < end {
+		nums[start], nums[end] = nums[end], nums[start]
+		start++
+		end--
+	}
 }
--- src/leetcode/m189/RotateArray_test.go
+++ src/leetcode/m189/RotateArray_test.go
@@ -11,13 +11,16 @@ func Test_rotate(t *testing.T) {
 		name     string
 		args     args
 		expected []int
+		rotator  Rotator
 	}{
-		{"Test case 1", args{[]int{1, 2, 3, 4, 5, 6, 7}, 3}, []int{5, 6, 7, 1, 2, 3, 4}},
-		{"Test case 2", args{[]int{-1, -100, 3, 99}, 2}, []int{3, 99, -1, -100}},
+		{"Test case 1 with Solution1", args{[]int{1, 2, 3, 4, 5, 6, 7}, 3}, []int{5, 6, 7, 1, 2, 3, 4}, Solution1{}},
+		{"Test case 2 with Solution1", args{[]int{-1, -100, 3, 99}, 2}, []int{3, 99, -1, -100}, Solution1{}},
+		{"Test case 1 with Solution2", args{[]int{1, 2, 3, 4, 5, 6, 7}, 3}, []int{5, 6, 7, 1, 2, 3, 4}, Solution2{}},
+		{"Test case 2 with Solution2", args{[]int{-1, -100, 3, 99}, 2}, []int{3, 99, -1, -100}, Solution2{}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			rotate(tt.args.nums, tt.args.k)
+			tt.rotator.Rotate(tt.args.nums, tt.args.k)
 			for i := range tt.args.nums {
 				if tt.args.nums[i] != tt.expected[i] {
 					t.Errorf("rotate() = %v, want %v", tt.args.nums, tt.expected)
