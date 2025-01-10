23f579c0e8a9d2be3cc2f91172e8ad207b548769
THGeek <alex24ht@hotmail>
2025-01-10 10:51:54 +0800

    feat: improve the Go solution of LeetCode 88 by decreasing time-complexity to O(m+n)

--- src/leetcode/e88/MergeSortedArray.go
+++ src/leetcode/e88/MergeSortedArray.go
@@ -1,11 +1,30 @@
 package e88
 
-import "sort"
-
 // LeetCode 88
 func merge(nums1 []int, m int, nums2 []int, n int) {
-	for i, j := m, 0; i < len(nums1); i, j = i+1, j+1 {
-		nums1[i] = nums2[j]
+	res := make([]int, len(nums1))
+	idx, i, j := 0, 0, 0
+	for i < m && j < n {
+		if nums1[i] <= nums2[j] {
+			res[idx] = nums1[i]
+			idx++
+			i++
+		} else {
+			res[idx] = nums2[j]
+			idx++
+			j++
+		}
 	}
-	sort.Sort(sort.IntSlice(nums1))
+
+	for i < m {
+		res[idx] = nums1[i]
+		idx++
+		i++
+	}
+	for j < n {
+		res[idx] = nums2[j]
+		idx++
+		j++
+	}
+	copy(nums1, res)
 }
