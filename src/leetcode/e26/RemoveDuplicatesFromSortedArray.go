0dac301cdb4a740dc3c8065a719ffc06f104f743
THGeek <alex24ht@hotmail>
2025-01-12 10:34:58 +0800

    feat: add another Go solution for LeetCode 26

--- src/leetcode/e26/RemoveDuplicatesFromSortedArray.go
+++ src/leetcode/e26/RemoveDuplicatesFromSortedArray.go
@@ -1,7 +1,30 @@
 package e26
 
 // LeetCode 26
-func removeDuplicates(nums []int) int {
+type RemoveDuplicatesFromSortedArray interface {
+	removeDuplicates(nums []int) int
+}
+
+type Solution1 struct {
+}
+
+func (s Solution1) removeDuplicates(nums []int) int {
+	idx := 1
+	prev := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != prev {
+			nums[idx] = nums[i]
+			prev = nums[i]
+			idx++
+		}
+	}
+	return idx
+}
+
+type Solution2 struct {
+}
+
+func (s Solution2) removeDuplicates(nums []int) int {
 	idx := 0
 	numMap := make(map[int]int)
 	for _, num := range nums {
--- src/leetcode/e26/RemoveDuplicatesFromSortedArray_test.go
+++ src/leetcode/e26/RemoveDuplicatesFromSortedArray_test.go
@@ -3,7 +3,6 @@ package e26
 import "testing"
 
 func Test_removeDuplicates(t *testing.T) {
-
 	tests := []struct {
 		name string
 		nums []int
@@ -12,11 +11,21 @@ func Test_removeDuplicates(t *testing.T) {
 		{"Test case 1", []int{1, 1, 2}, 2},
 		{"Test case 2", []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}, 5},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := removeDuplicates(tt.nums); got != tt.want {
-				t.Errorf("removeDuplicates() = %v, want %v", got, tt.want)
-			}
-		})
+
+	solutions := []RemoveDuplicatesFromSortedArray{
+		Solution1{},
+		Solution2{},
+	}
+
+	for _, sol := range solutions {
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				numsCopy := make([]int, len(tt.nums))
+				copy(numsCopy, tt.nums)
+				if got := sol.removeDuplicates(numsCopy); got != tt.want {
+					t.Errorf("%T.removeDuplicates() = %v, want %v", sol, got, tt.want)
+				}
+			})
+		}
 	}
 }
