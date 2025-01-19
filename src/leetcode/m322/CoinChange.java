760c5042f4f4035ac2081e619af620b886c4d10e
THGeek <alex24ht@hotmail>
2025-01-19 11:01:01 +0800

    perf: improve the solution for LeetCode 322

--- src/leetcode/m322/CoinChange.java
+++ src/leetcode/m322/CoinChange.java
@@ -16,8 +16,8 @@ import java.util.Arrays;
 public class CoinChange {
     public int coinChange(int[] coins, int amount) {
         Arrays.sort(coins);
-        var dp = new long[amount + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
+        var dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
         dp[0] = 0;
         for (int i = 1; i <= amount; i++) {
             for (int coin : coins) {
@@ -27,6 +27,6 @@ public class CoinChange {
                 dp[i] = Math.min(dp[i], dp[i - coin] + 1);
             }
         }
-        return dp[amount] == Integer.MAX_VALUE ? -1 : (int) dp[amount];
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
     }
 }
