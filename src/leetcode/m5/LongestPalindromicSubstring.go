1e48373db37d0b4badb0cf642cbff7aa79587f5d
THGeek <alex24ht@hotmail>
2025-01-25 14:08:40 +0800

    feat: add the Go solution for LeetCode 5

---
+++ src/leetcode/m5/LongestPalindromicSubstring.go
@@ -0,0 +1,28 @@
+package m5
+
+// LeetCode 5
+func longestPalindrome(s string) string {
+	res := ""
+	for i := 0; i < len(s); i++ {
+		str1 := longest(s, i, i)
+		str2 := longest(s, i, i+1)
+		resTemp := ""
+		if len(str1) > len(str2) {
+			resTemp = str1
+		} else {
+			resTemp = str2
+		}
+		if len(resTemp) > len(res) {
+			res = resTemp
+		}
+	}
+	return res
+}
+
+func longest(s string, l, r int) string {
+	for l >= 0 && r < len(s) && string(s[l]) == string(s[r]) {
+		l--
+		r++
+	}
+	return s[l+1 : r]
+}
