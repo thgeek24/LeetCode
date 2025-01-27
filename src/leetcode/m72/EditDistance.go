69366828e8a7eef7aa950d3a6504ebe18c7ada30
THGeek <alex24ht@hotmail>
2025-01-27 10:51:48 +0800

    feat: add the Go solution for LeetCode 72

---
+++ src/leetcode/m72/EditDistance.go
@@ -0,0 +1,42 @@
+package m72
+
+// LeetCode 72
+func minDistance(word1 string, word2 string) int {
+	m, n := len(word1), len(word2)
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+
+	for i := 1; i <= m; i++ {
+		dp[i][0] = i
+	}
+	for j := 1; j <= n; j++ {
+		dp[0][j] = j
+	}
+
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if word1[i-1] == word2[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				dp[i][j] = minInt(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+			}
+		}
+	}
+	return dp[m][n]
+}
+
+func minInt(a, b, c int) int {
+	if a < b {
+		if a < c {
+			return a
+		}
+		return c
+	}
+
+	if b < c {
+		return b
+	}
+	return c
+}
