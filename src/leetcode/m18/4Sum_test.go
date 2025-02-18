3d8fbc971e0910e1ce28797fe439a5a99b8254a0
THGeek <alex24ht@hotmail>
2025-02-18 11:03:38 +0800

    feat: add Go solution for LeetCode 18

---
+++ src/leetcode/m18/4Sum_test.go
@@ -0,0 +1,28 @@
+package m18
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_fourSum(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{"Test case 1", args{[]int{1, 0, -1, 0, -2, 2}, 0}, [][]int{{-2, -1, 1, 2}, {-2, 0, 0, 2}, {-1, 0, 0, 1}}},
+		{"Test case 2", args{[]int{2, 2, 2, 2, 2}, 8}, [][]int{{2, 2, 2, 2}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fourSum(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("fourSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
