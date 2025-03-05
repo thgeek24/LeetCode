59141c692c12d38ca7db6c325d217de207a2fc79
THGeek <alex24ht@hotmail>
2025-03-05 10:39:21 +0800

    feat: add solution and tests for LeetCode 1008

---
+++ src/hsbc/mock/PreorderTraversalOfABST.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2025 THGEEK.COM. All rights reserved.
+ */
+
+package hsbc.mock;
+
+import leetcode.m1008.ConstructBinarySearchTreeFromPreorderTraversal;
+
+/**
+ * HSBC Mock from naukri.com.
+ * <br/>
+ * See {@link ConstructBinarySearchTreeFromPreorderTraversal}
+ *
+ * @author Tao Hong
+ * @version 1.0
+ * @since 2025/03/04 17:35
+ */
+public class PreorderTraversalOfABST {
+
+}
