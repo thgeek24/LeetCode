11cc475ec652e370706cb87d70a5d0e341a060c0
THGeek <alex24ht@hotmail>
2025-03-15 10:25:26 +0800

    feat: add a correct GO solution for LeetCode 224

--- src/leetcode/h224/BasicCalculator.go
+++ src/leetcode/h224/BasicCalculator.go
@@ -1,69 +1,51 @@
 package h224
 
-import (
-	"strconv"
-	"strings"
-)
-
 // LeetCode 224
 func calculate(s string) int {
-	stack := make([]string, 0)
-	var builder strings.Builder
-	for _, c := range s {
-		switch c {
-		case ' ':
-			continue
-		case ')':
-			stack = append(stack, builder.String())
-			builder.Reset()
-			tempRes := calcStack(&stack)
-			stack = append(stack, string(rune(tempRes)))
-		case '(':
-			stack = append(stack, builder.String())
-			builder.Reset()
-			stack = append(stack, string(c))
-		case '+':
-			if builder.Len() > 0 {
-				stack = append(stack, builder.String())
-				builder.Reset()
-			}
-		case '-':
-			if builder.Len() > 0 {
-				stack = append(stack, builder.String())
-				builder.Reset()
-			}
-			builder.WriteRune(c)
-		default:
-			builder.WriteRune(c)
-		}
-	}
-	if builder.Len() > 0 {
-		stack = append(stack, builder.String())
-	}
-	res := 0
-	for _, numStr := range stack {
-		num, _ := strconv.Atoi(numStr)
-		res += num
-	}
-	return res
-}
+	var stack []int
+	result, sign := 0, 1
+	n := len(s)
+	i := 0
 
-func calcStack(stack *[]string) int {
-	res := 0
-	s := *stack
-	for s[len(s)-1] != "(" {
-		c := s[len(s)-1]
-		s = s[:len(s)-1]
-		num, _ := strconv.Atoi(c)
-		res += num
+	for i < n {
+		ch := s[i]
+		switch ch {
+		case ' ':
+			// Skip spaces
+		case '+':
+			sign = 1
+		case '-':
+			sign = -1
+		case '(':
+			// Push the current result and sign onto the stack
+			stack = append(stack, result)
+			stack = append(stack, sign)
+			// Reset result and sign for the new sub-expression
+			result, sign = 0, 1
+		case ')':
+			// Pop the sign and previous result from the stack
+			if len(stack) > 0 {
+				prevSign := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				prevResult := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				result = prevResult + prevSign*result
+			}
+		default:
+			// If the character is a digit, accumulate the number.
+			if ch >= '0' && ch <= '9' {
+				num := 0
+				for i < n && s[i] >= '0' && s[i] <= '9' {
+					num = num*10 + int(s[i]-'0')
+					i++
+				}
+				result += sign * num
+				// Continue here to avoid extra i++ since we already moved i in the inner loop.
+				continue
+			}
+		}
+		i++
 	}
-	s = s[:len(s)-1]
-	if len(s) > 0 && s[len(s)-1] == "-" {
-		s = s[:len(s)-1]
-		return res * -1
-	}
-	if len(s) > 0 && s[len(s)-1] == "+" {
-		s = s[:len(s)-1]
-	}
-	return res
+
+	return result
 }
