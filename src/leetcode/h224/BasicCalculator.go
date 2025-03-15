7e4341cd6bf3c5731b5becd5328d29d82fdd1836
THGeek <alex24ht@hotmail>
2025-03-15 12:00:07 +0800

    perf: implement the GO solution for LeetCode 224

--- src/leetcode/h224/BasicCalculator.go
+++ src/leetcode/h224/BasicCalculator.go
@@ -2,50 +2,42 @@ package h224
 
 // LeetCode 224
 func calculate(s string) int {
-	var stack []int
-	result, sign := 0, 1
+	stack := make([]int, 0)
+	res, sign := 0, 1
 	n := len(s)
 	i := 0
 
 	for i < n {
-		ch := s[i]
-		switch ch {
+		c := s[i]
+		switch c {
 		case ' ':
-			// Skip spaces
 		case '+':
 			sign = 1
 		case '-':
 			sign = -1
 		case '(':
-			// Push the current result and sign onto the stack
-			stack = append(stack, result)
+			stack = append(stack, res)
 			stack = append(stack, sign)
-			// Reset result and sign for the new sub-expression
-			result, sign = 0, 1
+			res, sign = 0, 1
 		case ')':
-			// Pop the sign and previous result from the stack
 			if len(stack) > 0 {
 				prevSign := stack[len(stack)-1]
 				stack = stack[:len(stack)-1]
-				prevResult := stack[len(stack)-1]
+				prevRes := stack[len(stack)-1]
 				stack = stack[:len(stack)-1]
-				result = prevResult + prevSign*result
+				res = prevRes + prevSign*res
 			}
 		default:
-			// If the character is a digit, accumulate the number.
-			if ch >= '0' && ch <= '9' {
-				num := 0
-				for i < n && s[i] >= '0' && s[i] <= '9' {
-					num = num*10 + int(s[i]-'0')
-					i++
-				}
-				result += sign * num
-				// Continue here to avoid extra i++ since we already moved i in the inner loop.
-				continue
+			num := 0
+			for i < n && s[i] >= '0' && s[i] <= '9' {
+				num = num*10 + int(s[i]-'0')
+				i++
 			}
+			res += num * sign
+			continue
 		}
 		i++
 	}
 
-	return result
+	return res
 }
