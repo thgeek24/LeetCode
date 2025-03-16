890fd94e0ca1a575858ca198f27c4e371dfc0d59
THGeek <alex24ht@hotmail>
2025-03-16 08:26:00 +0800

    feat: add solution and tests for LeetCode 141

---
+++ src/leetcode/e141/LinkedListCycle.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2025 THGEEK.COM. All rights reserved.
+ */
+
+package leetcode.e141;
+
+import leetcode.ListNode;
+
+/**
+ * LeetCode 141
+ *
+ * @author Tao Hong
+ * @version 1.0
+ * @since 2025/03/16 08:21
+ */
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        return true;
+    }
+}
