e5c839c378c3774eecb5b0ebd56303d1efc58c92
THGeek <alex24ht@hotmail>
2025-04-13 09:37:21 +0800

    feat: add the GO solution for LeetCode 106

---
+++ src/leetcode/m106/ConstructBinaryTreeFromInorderAndPostorderTraversal.go
@@ -0,0 +1,13 @@
+package m106
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// LeetCode 106
+func buildTree(inorder []int, postorder []int) *TreeNode {
+
+	return nil
+}
