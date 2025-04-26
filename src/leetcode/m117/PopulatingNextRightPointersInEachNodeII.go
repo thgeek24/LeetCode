a6524fb5b0806f3082eac3ecf3b38389f6b08502
THGeek <alex24ht@hotmail>
2025-04-26 22:53:24 +0800

    feat: add the GO solution for LeetCode 117

--- src/leetcode/m117/PopulatingNextRightPointersInEachNodeII.go
+++ src/leetcode/m117/PopulatingNextRightPointersInEachNodeII.go
@@ -9,6 +9,26 @@ type Node struct {
 
 // LeetCode 117
 func connect(root *Node) *Node {
+	if root == nil {
+		return root
+	}
 
-	return nil
+	nodeMap := make(map[int]*Node)
+	connectNodeMap(root, 0, &nodeMap)
+	return root
+}
+
+func connectNodeMap(node *Node, layer int, nodeMap *map[int]*Node) {
+	if node == nil {
+		return
+	}
+
+	if curr, exist := (*nodeMap)[layer]; exist {
+		curr.Next = node
+		(*nodeMap)[layer] = curr.Next
+	} else {
+		(*nodeMap)[layer] = node
+	}
+	connectNodeMap(node.Left, layer+1, nodeMap)
+	connectNodeMap(node.Right, layer+1, nodeMap)
 }
