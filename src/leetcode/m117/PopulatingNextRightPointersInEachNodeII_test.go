a9e3a92b54de039a15d8da050686ba9b06a4dd55
THGeek <alex24ht@hotmail>
2025-04-27 12:02:25 +0800

    feat: remove incorrect test case

--- src/leetcode/m117/PopulatingNextRightPointersInEachNodeII_test.go
+++ src/leetcode/m117/PopulatingNextRightPointersInEachNodeII_test.go
@@ -11,13 +11,6 @@ func Test_connect(t *testing.T) {
 		root *Node
 		want *Node
 	}{
-		{"Test case 1", &Node{Val: 1,
-			Left:  &Node{Val: 2, Left: &Node{Val: 4}, Right: &Node{Val: 5}},
-			Right: &Node{Val: 3, Right: &Node{Val: 7}}},
-			&Node{Val: 1,
-				Left:  &Node{Val: 2, Left: &Node{Val: 4}, Right: &Node{Val: 5}},
-				Right: &Node{Val: 3, Right: &Node{Val: 7}}},
-		},
 		{"Test case 2", &Node{}, &Node{}},
 	}
 	for _, tt := range tests {
