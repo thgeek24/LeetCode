5d3afcf5ef920ba1e47f6ef2c60ac95162de51b4
THGeek <alex24ht@hotmail>
2025-05-02 18:09:53 +0800

    refactor: rename AverageOfLevelsInBinaryTree_637 to AverageOfLevelsInBinaryTree and update package structure

--- test/leetcode/e637/AverageOfLevelsInBinaryTreeTest.java
+++ test/leetcode/e637/AverageOfLevelsInBinaryTreeTest.java
@@ -1,25 +1,25 @@
 /*
- * Copyright (c) 2024 THGEEK.COM. All rights reserved.
+ * Copyright (c) 2024-2025 THGEEK.COM. All rights reserved.
  */
 
-package leetcode;
+package leetcode.e637;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
- * Test for {@link AverageOfLevelsInBinaryTree_637}
+ * Test for {@link AverageOfLevelsInBinaryTree}
  *
  * @author Tao Hong
  * @version 1.0
  * @since 2024/08/24 09:36
  */
-class AverageOfLevelsInBinaryTree_637Test {
-    private AverageOfLevelsInBinaryTree_637 test;
+class AverageOfLevelsInBinaryTreeTest {
+    private AverageOfLevelsInBinaryTree test;
 
     @BeforeEach
     void setUp() {
-        test = new AverageOfLevelsInBinaryTree_637();
+        test = new AverageOfLevelsInBinaryTree();
     }
 
     @Test
